4efb44e4e77f258695cbfd9195a63f5529e84d7f
Ryan Echols <rkechols21@gmail.com>
2021-10-09 01:34:34 -0600

    basic rectangle struct

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
-use std::io;
+// use std::io;
 
-fn main() {
-    let n = loop {
-        println!("Enter a positive integer:");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Could not read from stdin!");
-        let input: u8 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input; try again.");
-                continue;
-            }
-        };
-        if input <= 0 {
-            println!("Number must be positive; try again.");
-            continue;
-        }
-        break input;
-    };
-
-    println!("Calculating the n-th Fibonacci number, n = {}", n);
-    let result = nth_fib(n);
-    println!("Your Fibonacci number is {}", result)
+struct Rectangle {
+    width: i32,
+    height: i32
 }
 
-fn nth_fib(n: u8) -> u128 {
-    if n < 3 {
-        return 1;
-    }
-    let mut num_old: u128 = 1;
-    let mut num_new: u128 = 1;
-    for _ in 3..=n {
-        let num_next = num_old.checked_add(num_new).expect("Integer overflow! Number is too big.");
-        num_old = num_new;
-        num_new = num_next;
-    }
-    num_new
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50
+    };
+    let area = calc_area(&rect);
+    println!("The area of a {} x {} rectangle is {}", rect.width, rect.height, area)
+}
+
+fn calc_area(rectangle: &Rectangle) -> i32 {
+    rectangle.width * rectangle.height
 }
